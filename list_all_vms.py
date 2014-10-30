12d577304d9efc6a272c19120b568bdca0efafa1
Jason Friedrich <jason@friedrich>
2014-10-31 00:36:21 +0100

    Script modifications
    Made several script modifications for more security and cleaner output. Details as follows:
    
    list_all_vms.py:
    - Added snarky remarks (aka comments)
    - Modified argument parsing
     - Server and username arguments are now mandatory
     - Password argument is no optional, will be asked if omitted on the cmd line
    - Added verbose argument
    - Printing data center name(s) only when verbose is set
    - Disabled warnings about self-signed certificate during connect
    
    list_vms_with_snapshots.py:
    - Added snarky remarks (aka comments)
    - Modified argument parsing
     - Server and username arguments are now mandatory
     - Password argument is no optional, will be asked if omitted on the cmd line
    - Added verbose argument
    - Printing data center name(s) only when verbose is set
    - Changed output format. Now boxed as optimized for fixed width ASCII (for example to send auto-generated via email every n days)
    - Disabled warnings about self-signed certificate during connect

--- list_all_vms.py
+++ list_all_vms.py
@@ -1,28 +1,49 @@
 #!/usr/bin/env python
 
+# Importing required modules
 import argparse
 import sys
+import getpass
 from pyVim import connect
 from pyVmomi import vim
+import requests             # Imported to disable SSL warning
 
-parser = argparse.ArgumentParser(prog='list_all_vms.py')
-parser.add_argument("server", help="vCenter server name or IP address")
-parser.add_argument("username", help="vCenter username")
-parser.add_argument("password", help="vCenter password")
+# Parsing arguments
+parser = argparse.ArgumentParser(prog='list_vms_with_snapshots.py')
+parser.add_argument('-s', '--server', help='vCenter server name or IP address', required=True)
+parser.add_argument('-u', '--username', help='vCenter username', required=True)
+parser.add_argument('-p', '--password', help='vCenter password')
+parser.add_argument('-v', '--verbose', help='more information displayed')
+
 args = parser.parse_args()
 
+verbose = args.verbose
 hostname = args.server
 username = args.username
 passwd = args.password
 
+# Asking user for password if not specified on the commandline
+if passwd is None:
+    passwd = getpass.getpass()
+
+# Since all my VMware instances are using self-signed certificates, to which
+# I do not necessarily have access to the root CA, SSL warnings are disabled
+requests.packages.urllib3.disable_warnings()
+
+# Connectign to vCenter
 instance = connect.SmartConnect(host=hostname,
                                 user=username,
                                 pwd=passwd)
-
+# Getting instance content
 content = instance.RetrieveContent()
 
+# Accessing datacenter folder directly under root folder. For more information
+# about the hierarchy, see
+# http://vmware.github.io/pyvmomi-community-samples/assets/vchierarchy.pdf
 datacenters = content.rootFolder.childEntity
 
+# Function to display VM information. stupid tabs variable included, that 
+# is supposed to help displaying folder hierarchy. Working on new output format
 def list_vm_info(virtual_machine, tabs=1):
     summary = virtual_machine.summary
     print tabs * 3 * ' ' + '-> ' + summary.config.name
@@ -37,63 +58,78 @@ def list_vm_info(virtual_machine, tabs=1):
     print tabs * 3 * ' ' + '   ' + 'VM path          :', summary.config.vmPathName
     print tabs * 3 * ' ' + '   ' + 'Power state      :', summary.runtime.powerState
     
+    # If the object has a currentSnapshot, we want to decend the hierarchy down
+    # max 3 levels (root snapshot + 2 additional snapshots). If there is more,
+    # the user needs to take a look with the vSphere client    
     snapshot = virtual_machine.snapshot
     if hasattr(snapshot, 'currentSnapshot'):
         print tabs * 3 * ' ' + '   ' + 'Snapshots        : Yes'
+        # Getting info about the root snapshot and displaying it
         root_snapshot = snapshot.rootSnapshotList[0]
         root_snapshot_name = root_snapshot.name
         root_snapshot_date = root_snapshot.createTime
         print tabs * 3 * ' ' + '   ' + 'Latest snapshots :'
         print tabs * 3 * ' ' + '     \\- ' + "%s: %s" % (root_snapshot_date,
                                                          root_snapshot_name)
-                                                           
+        # If the root snapshot has children, lets see its ugly faces
         if (root_snapshot.childSnapshotList):
             for first_snapshot in root_snapshot.childSnapshotList:
                 print tabs * 3 * ' ' + '       \\- ' + "%s: %s" % (first_snapshot.createTime,
                                                                    first_snapshot.name)                
+                # Let's see if the first snapshots has any child objects
                 if (first_snapshot.childSnapshotList):
                     for second_snapshot in first_snapshot.childSnapshotList:
                         print tabs * 3 * ' ' + '         \\- ' + "%s: %s" % (second_snapshot.createTime,
                                                                              second_snapshot.name)
+                        # If there are more snapshots, go use the vSphere Client
                         if (second_snapshot.childSnapshotList):
                             print tabs * 3 * ' ' + '           !!  ' + 'WARNING: Only 3 levels of' \
                             ' snapshots supported, but this Virtual Machine has more'
-
+    # VM has no snapshots :(
     else:
         print tabs * 3 * ' ' + '   ' + 'Snapshots        : No'
-
+    # We need some distance (divider)
     print ""
-    
+
+# Display the folder names in a funky way.
+# The 90's called, they want their ASCII back    
 def print_folder_name(folder, tabs=1):
     print tabs * 2 * ' ' + '== ' + folder.name + "\n"
-    
+
+# We just descend recursively until we reached max depth of 20
 def identify_item(data_item, depth=1):
     maxdepth = 20
     if depth > maxdepth:
         return            
 
+    # If the found object is a folder, we want to print it's name
     if isinstance(data_item, vim.Folder):
         print_folder_name(data_item, depth)
-        
+
+    # If the object is a virtual machine, we want to display it's information
     elif isinstance(data_item, vim.VirtualMachine):
         list_vm_info(data_item, depth)
-        
+
+    # If the object has child entities, we follow down the rabbit hole until we
+    # reach maxdepth and identify every object on our way
     if hasattr(data_item, 'childEntity'):
         data_item_list = data_item.childEntity
         for item in data_item_list:
             identify_item(item, depth + 1)
         return
 
-
 # Start program
 if __name__ == "__main__":
     for datacenter in datacenters:
-        dc_name = datacenter.name
+        if verbose:
+            dc_name = datacenter.name
+            print "\n--== Datacenter:", dc_name, "==--\n"
+
         vm_folder = datacenter.vmFolder
         item_list = vm_folder.childEntity
-        print "\n--== Datacenter:", dc_name, "==--\n"
     
         for item in item_list:
             identify_item(item)
-        
+
+    # Clean exit    
     sys.exit(0)
--- list_vms_with_snapshots.py
+++ list_vms_with_snapshots.py
@@ -1,99 +1,129 @@
 #!/usr/bin/env python
 
+# Importing required modules
 import argparse
 import sys
+import getpass
 from pyVim import connect
 from pyVmomi import vim
+import requests             # Imported to disable SSL warning
 
-parser = argparse.ArgumentParser(prog='list_all_vms.py')
-parser.add_argument("server", help="vCenter server name or IP address")
-parser.add_argument("username", help="vCenter username")
-parser.add_argument("password", help="vCenter password")
+# Parsing arguments
+parser = argparse.ArgumentParser(prog='list_vms_with_snapshots.py')
+parser.add_argument('-s', '--server', help='vCenter server name or IP address', required=True)
+parser.add_argument('-u', '--username', help='vCenter username', required=True)
+parser.add_argument('-p', '--password', help='vCenter password')
+parser.add_argument('-v', '--verbose', help='more information displayed')
+
 args = parser.parse_args()
 
+verbose = args.verbose
 hostname = args.server
 username = args.username
 passwd = args.password
 
+# Asking user for password if not specified on the commandline
+if passwd is None:
+    passwd = getpass.getpass()
+
+# Since all my VMware instances are using self-signed certificates, to which
+# I do not necessarily have access to the root CA, SSL warnings are disabled
+requests.packages.urllib3.disable_warnings()
+
+# Connectign to vCenter
 instance = connect.SmartConnect(host=hostname,
                                 user=username,
                                 pwd=passwd)
-
+# Getting instance content
 content = instance.RetrieveContent()
 
+# Accessing datacenter folder directly under root folder. For more information
+# about the hierarchy, see
+# http://vmware.github.io/pyvmomi-community-samples/assets/vchierarchy.pdf
 datacenters = content.rootFolder.childEntity
 
-def list_vm_info(virtual_machine, tabs=1):
-    summary = virtual_machine.summary
-    print tabs * 3 * ' ' + '-> ' + summary.config.name
-    print tabs * 3 * ' ' + '   ' + 'Distribution     :', summary.config.guestFullName
-    print tabs * 3 * ' ' + '   ' + 'Hostname         :', summary.guest.hostName
-    print tabs * 3 * ' ' + '   ' + 'UUID             :', summary.config.uuid
-    print tabs * 3 * ' ' + '   ' + 'IP address       :', summary.guest.ipAddress
-    print tabs * 3 * ' ' + '   ' + 'vCPU             :', summary.config.numCpu
-    print tabs * 3 * ' ' + '   ' + 'vRAM (MB)        :', summary.config.memorySizeMB
-    print tabs * 3 * ' ' + '   ' + 'Ethernet cards   :', summary.config.numEthernetCards
-    print tabs * 3 * ' ' + '   ' + 'vDisks           :', summary.config.numVirtualDisks
-    print tabs * 3 * ' ' + '   ' + 'VM path          :', summary.config.vmPathName
-    print tabs * 3 * ' ' + '   ' + 'Power state      :', summary.runtime.powerState
-    
+# Function to look through all objects and identify Virtual Machines
+def find_vms_with_snapshots(item):
+    if isinstance(item, vim.VirtualMachine):
+        if check_for_snapshots(item):
+            display_snapshot_info(item)
+        
+    if hasattr(item, 'childEntity'):
+        item_list = item.childEntity
+        for subitem in item_list:
+            find_vms_with_snapshots(subitem)
+
+# Check if the Virtual Machine has any snapshots
+def check_for_snapshots(virtual_machine):
     snapshot = virtual_machine.snapshot
     if hasattr(snapshot, 'currentSnapshot'):
-        print tabs * 3 * ' ' + '   ' + 'Snapshots        : Yes'
-        root_snapshot = snapshot.rootSnapshotList[0]
-        root_snapshot_name = root_snapshot.name
-        root_snapshot_date = root_snapshot.createTime
-        print tabs * 3 * ' ' + '   ' + 'Latest snapshots :'
-        print tabs * 3 * ' ' + '     \\- ' + "%s: %s" % (root_snapshot_date,
-                                                         root_snapshot_name)
-                                                           
-        if (root_snapshot.childSnapshotList):
-            for first_snapshot in root_snapshot.childSnapshotList:
-                print tabs * 3 * ' ' + '       \\- ' + "%s: %s" % (first_snapshot.createTime,
-                                                                   first_snapshot.name)                
-                if (first_snapshot.childSnapshotList):
-                    for second_snapshot in first_snapshot.childSnapshotList:
-                        print tabs * 3 * ' ' + '         \\- ' + "%s: %s" % (second_snapshot.createTime,
-                                                                             second_snapshot.name)
-                        if (second_snapshot.childSnapshotList):
-                            print tabs * 3 * ' ' + '           !!  ' + 'WARNING: Only 3 levels of' \
-                            ' snapshots supported, but this Virtual Machine has more'
+        return True
 
-    else:
-        print tabs * 3 * ' ' + '   ' + 'Snapshots        : No'
-
+# Display Virtual Machine and Snapshot information
+def display_snapshot_info(virtual_machine):
+    summary = virtual_machine.summary
+    snapshot = virtual_machine.snapshot
+    
+    # We want at least the VM name and it's UUID
+    vm_name = summary.config.name
+    vm_uuid = summary.config.uuid
+    print ',' + 79 * '-'
+    print '| Virtual Machine :', vm_name
+    print '| VMware UUID     :', vm_uuid
+    print '|' + 79 * '-'
+    
+    # Getting information about the root snapshot
+    root_snapshot = snapshot.rootSnapshotList[0]
+    root_snapshot_name = root_snapshot.name
+    root_snapshot_date = root_snapshot.createTime
+    print '|' + 36 * ' ' + 'Snapshots'
+    print '|' + 79 * '-'
+    print '|' + 17 * ' ' + 'Date' + 19 * ' ' + '|' + 16 * ' ' + 'Name'
+    print '|' + 79 * '-'    
+    print '| ' + str(root_snapshot_date) + '       | ' + root_snapshot_name
+    # If there are child objects, we want information about them as well
+    if (root_snapshot.childSnapshotList):
+        for first_snapshot in root_snapshot.childSnapshotList:
+            first_snapshot_name = first_snapshot.name
+            first_snapshot_date = first_snapshot.createTime
+            print '| \\__', first_snapshot_date, '  |', first_snapshot_name
+            # Has the child object a child object? Grandkids! \ o /
+            if (first_snapshot.childSnapshotList):
+                for second_snapshot in first_snapshot.childSnapshotList:
+                    second_snapshot_name = second_snapshot.name
+                    second_snapshot_date = second_snapshot.createTime
+                    print '|   \\__', second_snapshot_date, '|', second_snapshot_name
+                    # If there are more snapshots, go use the vSphere Client
+                    if (second_snapshot.childSnapshotList):
+                        print '|' + 79 * '-'
+                        print '|  !! WARNING - Only 3 levels of snapshots ' \
+                        'supported, but this VM has more !!'
+    print '`' + 79 * '-'
     print ""
-    
-def print_folder_name(folder, tabs=1):
-    print tabs * 2 * ' ' + '== ' + folder.name + "\n"
-    
-def identify_item(data_item, depth=1):
-    maxdepth = 20
-    if depth > maxdepth:
-        return            
 
-    if isinstance(data_item, vim.Folder):
-        print_folder_name(data_item, depth)
-        
-    elif isinstance(data_item, vim.VirtualMachine):
-        list_vm_info(data_item, depth)
-        
-    if hasattr(data_item, 'childEntity'):
-        data_item_list = data_item.childEntity
-        for item in data_item_list:
-            identify_item(item, depth + 1)
-        return
+def print_dc_name(name):
+    print '//' + 78 * '='
+    print '|| Datacenter: ', name
+    print '\\\\' + 78 * '='
+    print ''
 
+# Coming soon. Optional display of days since snapshot was created instead of
+# a cryptic timestamp.
+def calculate_snapshot_age():
+    print 'Not implemented yet.'
 
 # Start program
 if __name__ == "__main__":
     for datacenter in datacenters:
-        dc_name = datacenter.name
+        if verbose:
+            dc_name = datacenter.name
+            print_dc_name(dc_name)
+
         vm_folder = datacenter.vmFolder
         item_list = vm_folder.childEntity
-        print "\n--== Datacenter:", dc_name, "==--\n"
-    
+
         for item in item_list:
-            identify_item(item)
-        
+            find_vms_with_snapshots(item)
+
+    # Clean exit
     sys.exit(0)
